67554ba41cec499184bf731352bf03a8aeda7b83
amaris-jpg <leusink@ualberta.ca>
2022-03-18 20:12:21 -0600

    added enterprises app to settings.py

--- ems/ems/enterprise/enterprise/settings.py
+++ ems/ems/enterprise/enterprise/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # My Apps
+    'enterprises',
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
