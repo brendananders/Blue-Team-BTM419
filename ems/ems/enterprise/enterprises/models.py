32b41149b3214ab596b22691b450fcd0ad7f28d2
amaris-jpg <leusink@ualberta.ca>
2022-03-22 10:33:32 -0600

    added inventory name field

--- ems/ems/enterprise/enterprises/models.py
+++ ems/ems/enterprise/enterprises/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Inventory(models.Model):
     serialNumber = models.IntegerField()
     upcCode = models.IntegerField(max_length=12)
+    name = models.CharField()
 
 class Warranty(models.Model):
     inventory = models.ForeignKey(Inventory)
