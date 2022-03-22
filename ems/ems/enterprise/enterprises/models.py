cc1abcdba0f0c406735c7c3d1b6f529651e90d61
amaris-jpg <leusink@ualberta.ca>
2022-03-22 12:39:47 -0600

    added statuses to claim class

--- ems/ems/enterprise/enterprises/models.py
+++ ems/ems/enterprise/enterprises/models.py
@@ -1,3 +1,4 @@
+from telnetlib import STATUS
 from django.db import models
 
 # Create your models here.
@@ -13,8 +14,20 @@ class Warranty(models.Model):
 class Claim(models.Model):
     dateSubmitted = models.DateField()
     warranty = models.ForeignKey(Warranty)
+    STATUS_CHOICES = [
+        ('AP', 'Approved'),
+        ('RJ', 'Rejected'),
+        ('AC', 'Active'),
+        ('PE', 'Pending')
+    ]
+    status = models.CharField(
+        max_length = 2,
+        choices = STATUS_CHOICES,
+        default = 'AC'
+    ) 
 
 class Inspection(models.Model):
+    inspectorName = models.CharField() # stored because the form can be filled out on someone's behalf
     date = models.DateField()
     approvalDate = models.DateField(blank=True, null=True)
     claim = models.ForeignKey(Claim)
