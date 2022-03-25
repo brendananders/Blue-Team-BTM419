eb520dd0cb155e737b44a7bab55b6a7c7a4643cc
amaris-jpg <leusink@ualberta.ca>
2022-03-25 13:44:44 -0600

    added entities to admin.py

--- ems/ems/enterprise/enterprises/admin.py
+++ ems/ems/enterprise/enterprises/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Dealership, Inventory, Warranty, Claim, Inspection
+
+admin.site.register(Dealership)
+admin.site.register(Inventory)
+admin.site.register(Warranty)
+admin.site.register(Claim)
+admin.site.register(Inspection)
