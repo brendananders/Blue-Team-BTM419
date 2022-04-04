d3fe17a799893ca78b0b9d9e5d464dec5a389b57
brendananders <branderson838@gmail.com>
2022-04-03 22:56:03 -0600

    commit of claims and warranties pages, more to come

--- ems/ems/enterprise/enterprises/models.py
+++ ems/ems/enterprise/enterprises/models.py
@@ -6,22 +6,22 @@ from django.db import models
 class Dealership(models.Model):
     name = models.CharField(max_length=30)
     address = models.CharField(max_length=80)
-    phoneNumber = models.IntegerField(max_length=10)
+    phoneNumber = models.IntegerField()
 
 class Inventory(models.Model):
-    serialNumber = models.IntegerField(max_length=50)
-    upcCode = models.IntegerField(max_length=12)
+    serialNumber = models.IntegerField()
+    upcCode = models.IntegerField()
     name = models.CharField(max_length=30)
-    dealership = models.ForeignKey(Dealership, on_delete=models.CASCADE)
+    dealership = models.ForeignKey(Dealership,on_delete=models.SET_NULL,null=True)
 
 class Warranty(models.Model):
-    inventory = models.ForeignKey(Inventory, on_delete=models.CASCADE)
+    inventory = models.ForeignKey(Inventory,on_delete=models.SET_NULL,null=True)
     brand = models.CharField(max_length=30)
     name = models.CharField(max_length=30)
 
 class Claim(models.Model):
     claimDate = models.DateField()
-    warranty = models.ForeignKey(Warranty, on_delete=models.CASCADE)
+    warranty = models.ForeignKey(Warranty,on_delete=models.SET_NULL,null=True)
     STATUS_CHOICES = [
         ('AP', 'Approved'),
         ('RE', 'Rejected'),
@@ -39,4 +39,4 @@ class Inspection(models.Model):
     inspectorName = models.CharField(max_length=30) # stored because the form can be filled out on someone's behalf
     inspectionDate = models.DateField(blank=True, null=True)
     dataEntryDate = models.DateField()
-    claim = models.ForeignKey(Claim, on_delete=models.CASCADE)
+    claim = models.ForeignKey(Claim,on_delete=models.SET_NULL,null=True)
