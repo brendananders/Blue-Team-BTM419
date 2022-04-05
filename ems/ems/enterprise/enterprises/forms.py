b738593c3e3edea240a20b7b0203c9c4662353e8
amaris-jpg <leusink@ualberta.ca>
2022-04-05 13:05:34 -0600

    tried and failed to add calendar functionality

--- ems/ems/enterprise/enterprises/forms.py
+++ ems/ems/enterprise/enterprises/forms.py
@@ -1,6 +1,7 @@
 #enterprise/forms.py
 from django import forms
-from .models import Claim, Warranty, Inspection
+from .models import Claim, Warranty, Inspection, Event
+from django.forms import ModelForm, DateInput
 
 class ClaimForm(forms.ModelForm):
     class Meta:
@@ -24,3 +25,20 @@ class InspectionForm(forms.ModelForm):
             'dataEntryDate': 'Date of Data Entry:',
             'claim': 'Claim:'
         }
+
+# copied from Hui Wen https://www.huiwenteo.com/normal/2018/07/29/django-calendar-ii.html
+class EventForm(ModelForm):
+  class Meta:
+    model = Event
+    # datetime-local is a HTML5 input type, format to make date time show on fields
+    widgets = {
+      'start_time': DateInput(attrs={'type': 'datetime-local'}, format='%Y-%m-%dT%H:%M'),
+      'end_time': DateInput(attrs={'type': 'datetime-local'}, format='%Y-%m-%dT%H:%M'),
+    }
+    fields = '__all__'
+
+  def __init__(self, *args, **kwargs):
+    super(EventForm, self).__init__(*args, **kwargs)
+    # input_formats to parse HTML5 datetime-local input to datetime field
+    self.fields['start_time'].input_formats = ('%Y-%m-%dT%H:%M',)
+    self.fields['end_time'].input_formats = ('%Y-%m-%dT%H:%M',)
--- ems/ems/enterprise/enterprises/views.py
+++ ems/ems/enterprise/enterprises/views.py
@@ -1,13 +1,15 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import *
-from .forms import ClaimForm, InspectionForm
+from .forms import ClaimForm, InspectionForm, EventForm
 from django.contrib.auth.decorators import login_required
 # copied from Hui Wenteo https://www.huiwenteo.com/normal/2018/07/24/django-calendar.html
-from datetime import datetime
-from django.http import HttpResponse
+from datetime import datetime, timedelta, date
+from django.http import HttpResponse, HttpResponseRedirect
 from django.views import generic
 from django.utils.safestring import mark_safe
 from .utils import Calendar
+from django.urls import reverse
+import calendar
 
 
 
@@ -87,24 +89,46 @@ def newInspection(request):
 
 class CalendarView(generic.ListView):
     model = Event
-    template_name = 'enterprises/inspectionCalendar.html'
+    template_name = 'cal/calendar.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-
-        # use today's date for the calendar
-        d = get_date(self.request.GET.get('day', None))
-
-        # Instantiate our calendar class with today's year and date
+        d = get_date(self.request.GET.get('month', None))
         cal = Calendar(d.year, d.month)
-
-        # Call the formatmonth method, which returns our calendar as a table
         html_cal = cal.formatmonth(withyear=True)
         context['calendar'] = mark_safe(html_cal)
+        context['prev_month'] = prev_month(d)
+        context['next_month'] = next_month(d)
         return context
 
-def get_date(req_day):
-    if req_day:
-        year, month = (int(x) for x in req_day.split('-'))
+def get_date(req_month):
+    if req_month:
+        year, month = (int(x) for x in req_month.split('-'))
         return date(year, month, day=1)
     return datetime.today()
+
+def prev_month(d):
+    first = d.replace(day=1)
+    prev_month = first - timedelta(days=1)
+    month = 'month=' + str(prev_month.year) + '-' + str(prev_month.month)
+    return month
+
+def next_month(d):
+    days_in_month = calendar.monthrange(d.year, d.month)[1]
+    last = d.replace(day=days_in_month)
+    next_month = last + timedelta(days=1)
+    month = 'month=' + str(next_month.year) + '-' + str(next_month.month)
+    return month
+
+def event(request, event_id=None):
+    instance = Event()
+    if event_id:
+        instance = get_object_or_404(Event, pk=event_id)
+    else:
+        instance = Event()
+
+    form = EventForm(request.POST or None, instance=instance)
+    if request.POST and form.is_valid():
+        form.save()
+        return HttpResponseRedirect(reverse('enterprises:inspectionCalendar'))
+    return render(request, 'enterprises/event.html', {'form': form})
