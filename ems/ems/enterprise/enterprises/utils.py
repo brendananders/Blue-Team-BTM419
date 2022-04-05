583c4d43d75ac8152316017b55a0735b7e748450
amaris-jpg <leusink@ualberta.ca>
2022-04-05 11:57:33 -0600

    created calendar

--- ems/ems/enterprise/enterprises/utils.py
+++ ems/ems/enterprise/enterprises/utils.py
@@ -34,7 +34,7 @@ class Calendar(HTMLCalendar):
 	def formatmonth(self, withyear=True):
 		events = Event.objects.filter(start_time__year=self.year, start_time__month=self.month)
 
-		cal = f'<table border="0" cellpadding="0" cellspacing="0" class="calendar">\n'
+		cal = f'<table border="1" cellpadding="3" cellspacing="3" class="calendar">\n'
 		cal += f'{self.formatmonthname(self.year, self.month, withyear=withyear)}\n'
 		cal += f'{self.formatweekheader()}\n'
 		for week in self.monthdays2calendar(self.year, self.month):
